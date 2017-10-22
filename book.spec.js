0ffacba99d1c43c5e6866dd4cb2a9ab05ebd3554
Warintorn yangkijkarn <58660138@go.buu.ac.th>
2017-10-22 14:41:27 +0700

    แต่หนูดูโค้ดอาจารย์

--- book.spec.js
+++ book.spec.js
@@ -2,7 +2,12 @@ function Booklookup(mocktest) {
     this.mocktest = mocktest
 
     this.serachAmazon = (isbn) => {
-        return this.authService(phone, password)
+        var obj = this.mocktest()
+        return {
+            bookName :'Color of the wind',
+            cover :'ewtwt.bng'
+            isbn: 'is123456789'
+        }
     }
 }
 
@@ -10,16 +15,16 @@ test('Test Booklookup'=>(){
   var info  {
         title: 'Color of the wind',
         image: 'thevoice.jpg',
-        isbn: '123456789'
+        isbn: 'is123456789'
     }
   const AmazonService =  jest.fn().mockReturnValue(info)
 
 
   let app = new serach(AmazonService)
-  let value = app.signInWithFacebook('is2539')
+  let value = app.signInWithFacebook('is123456789')
 
   expect(value).toEqual(returnObject)
   expect(mockFacbook).toBeCalled()
-    expect(mockFacbook).toBeCalledWith('is2539')
+    expect(mockFacbook).toBeCalledWith('is123456789')
 
 })
